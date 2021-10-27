4a62c60f0cfc4146585c7b200dbc95736e3cc85e
pooh <cktjdgus45@naver.com>
2021-10-27 22:15:30 +0900

    code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,17 @@ import styles from './app.module.css';
 
 
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${query}&key=AIzaSyBhDQVi2NUEy10Lv1y3NL2H4241A1siiEM`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({ ...item, id: item.id.videoId })))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube //
+      .search(query)
+      .then(videos => setVideos(videos));
   }
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&type=video&maxResults=25&key=AIzaSyBhDQVi2NUEy10Lv1y3NL2H4241A1siiEM", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube //
+      .mostPopular()
+      .then(videos => setVideos(videos));
   }, [])
   return (
     <div className={styles.app}>
