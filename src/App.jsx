05653538cc24fd9665b1e7168fe5ad032b6f28fe
pooh <cktjdgus45@naver.com>
2021-10-27 20:05:22 +0900

    add video search func

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,32 @@ import styles from './app.module.css';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
 
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${query}&key=AIzaSyBhDQVi2NUEy10Lv1y3NL2H4241A1siiEM`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({ ...item, id: item.id.videoId })))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
 
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBhDQVi2NUEy10Lv1y3NL2H4241A1siiEM", requestOptions)
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&type=video&maxResults=25&key=AIzaSyBhDQVi2NUEy10Lv1y3NL2H4241A1siiEM", requestOptions)
       .then(response => response.json())
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
   }, [])
   return (
     <div className={styles.app}>
-      <SearchHeader></SearchHeader>
+      <SearchHeader onSearch={search}></SearchHeader>
       <VideoList videos={videos}></VideoList>
     </div>
   )
