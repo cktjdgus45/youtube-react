f910c48513b12dd0eb77151aa3bf43b18c0d3635
pooh <cktjdgus45@naver.com>
2021-10-28 21:02:52 +0900

    video detail

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import React, { useEffect, useState } from 'react';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
 import styles from './app.module.css';
+import VideoDetail from './components/video_detail/video_detail';
 
 
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState([]);
   const search = query => {
     youtube //
       .search(query)
       .then(videos => setVideos(videos));
   }
+
+  const selectVideo = video => {
+    setSelectedVideo(video);
+  }
+
   useEffect(() => {
     youtube //
       .mostPopular()
@@ -20,7 +27,17 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search}></SearchHeader>
-      <VideoList videos={videos}></VideoList>
+      <section className={styles.content}>
+        {
+          selectedVideo.id &&
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        }
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo.id ? 'list' : 'grid'}></VideoList>
+        </div>
+      </section>
     </div>
   )
 }
